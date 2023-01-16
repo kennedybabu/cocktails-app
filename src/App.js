a2e0a5923a0a56ed0d655e2c5a62b062bc965c3b
kennedybabu <kennedynjogu8@gmail.com>
2023-01-16 08:17:27 +0300

    feature: add routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-
+import { Route, Routes } from "react-router-dom";
+import Home from './pages/Home'
+import Cocktail from './pages/Cocktail'
 
 function App() {
   return (
     <div>
       <p className="text-xl">heello</p>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/cocktail/:id' element={<Cocktail />} />
+      </Routes>
     </div>
   );
 }
